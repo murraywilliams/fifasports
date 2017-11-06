463586b4798ea98c3c8503905395a386521222d5
Murray Williams <murray.williams@ventureweb.com>
2017-11-06 18:33:14 +0200

    added ability to create a new player and push to Firebase

--- fifa-sports/src/App.js
+++ fifa-sports/src/App.js
@@ -3,6 +3,38 @@ import firebase from './firebase.js';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      playerName: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // create space in FDB to store form submits
+    const playersRef = firebase.database().ref('players');
+    // grab values inputted into form
+    const player = {
+      name: this.state.playerName
+    }
+    // send copy of object up to Firebase
+    playersRef.push(player);
+    // clear out form input
+    this.setState({
+      playerName: '',
+    });
+  }
+
   render() {
     return (
         <div className='app'>
@@ -15,8 +47,8 @@ class App extends Component {
         <div className='container'>
           <section className='add-item'>
             <h3>Create new Player</h3>
-              <form>
-                <input type="text" name="playerName" placeholder="Enter a player name" />
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" name="playerName" placeholder="Enter a player name" onChange={this.handleChange} value={this.state.playerName} />
                 <button>Add Player</button>
               </form>
           </section>
