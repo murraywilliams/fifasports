3a338901caea83da64eecaca2e18852d06780630
Murray Williams <murray.williams@ventureweb.com>
2017-11-06 21:07:58 +0200

    functionality to list & remove players, fa icons and some basic card styling

--- fifa-sports/src/App.js
+++ fifa-sports/src/App.js
@@ -8,17 +8,19 @@ class App extends Component {
     super();
     this.state = {
       playerName: '',
+      players: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
+  // manage changes on our form input
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
   }
-
+  // method to grab form values and push to Firebase on
+  // submit of our form
   handleSubmit(e) {
     e.preventDefault();
     // create space in FDB to store form submits
@@ -33,6 +35,28 @@ class App extends Component {
     this.setState({
       playerName: '',
     });
+  }
+
+  componentDidMount() {
+    const playersRef = firebase.database().ref('players');
+    playersRef.on('value', (snapshot) => {
+      let players = snapshot.val();
+      let newState = [];
+      for (let player in players) {
+        newState.push({
+          id: player,
+          name: players[player].name
+        });
+      }
+      this.setState({
+        players: newState
+      });
+    });
+  }
+
+  removePlayer(playerId) {
+    const playerRef = firebase.database().ref(`/players/${playerId}`);
+    playerRef.remove();
   }
 
   render() {
@@ -51,6 +75,21 @@ class App extends Component {
                 <input type="text" name="playerName" placeholder="Enter a player name" onChange={this.handleChange} value={this.state.playerName} />
                 <button>Add Player</button>
               </form>
+          </section>
+          <section className='display-item'>
+              <div className="wrapper">
+                <ul>
+                  {this.state.players.map((player) => {
+                    return (
+                      <li key={player.id}>
+                        <h3>{player.name}</h3>
+                          <button class="edit--btn" onClick={() => this.removePlayer(player.id)}><span class="fa fa-lg fa-edit"></span></button>
+                          <button class="delete--btn" onClick={() => this.removePlayer(player.id)}><span class="fa fa-lg fa-trash"></span></button>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
           </section>
           </div>
       </div>
